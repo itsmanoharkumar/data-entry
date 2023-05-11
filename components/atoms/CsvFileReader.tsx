6751ca0e2c28f8b029559fb422636c4e4d3f8543
Manohar <itsmanoharkumar@gmail.com>
2023-05-11 10:46:15 +0530

    Changed UI of File uploader

--- components/atoms/CsvFileReader.tsx
+++ components/atoms/CsvFileReader.tsx
@@ -1,5 +1,6 @@
 import Papa from "papaparse";
 import { useState } from "react";
+import { Button } from "@mui/material";
 
 interface Props {
   onComplete: (data: any) => void;
@@ -10,26 +11,50 @@ export default function CsvFileReader({ onComplete }: Props) {
   return (
     <div className={""}>
       <div className={"font-medium mb-2"}>Upload CSV File</div>
-      <input
-        type="file"
-        accept=".csv"
-        onChange={(event) => {
-          if (event.target.files) {
-            Papa.parse(event.target.files[0], {
-              header: true,
-              skipEmptyLines: true,
-              complete: function (results) {
-                try {
-                  setData(JSON.stringify(results.data, null, 2));
-                } catch (e) {
-                  setData("Invalid CSV file");
-                }
-                onComplete(results.data);
-              },
-            });
-          }
-        }}
-      />
+      <Button variant="contained" component="label">
+        Upload
+        <input
+          hidden
+          accept=".csv"
+          type="file"
+          onChange={(event) => {
+            if (event.target.files) {
+              Papa.parse(event.target.files[0], {
+                header: true,
+                skipEmptyLines: true,
+                complete: function (results) {
+                  try {
+                    setData(JSON.stringify(results.data, null, 2));
+                  } catch (e) {
+                    setData("Invalid CSV file");
+                  }
+                  onComplete(results.data);
+                },
+              });
+            }
+          }}
+        />
+      </Button>
+      {/*<input*/}
+      {/*  type="file"*/}
+      {/*  accept=".csv"*/}
+      {/*  onChange={(event) => {*/}
+      {/*    if (event.target.files) {*/}
+      {/*      Papa.parse(event.target.files[0], {*/}
+      {/*        header: true,*/}
+      {/*        skipEmptyLines: true,*/}
+      {/*        complete: function (results) {*/}
+      {/*          try {*/}
+      {/*            setData(JSON.stringify(results.data, null, 2));*/}
+      {/*          } catch (e) {*/}
+      {/*            setData("Invalid CSV file");*/}
+      {/*          }*/}
+      {/*          onComplete(results.data);*/}
+      {/*        },*/}
+      {/*      });*/}
+      {/*    }*/}
+      {/*  }}*/}
+      {/*/>*/}
       <div
         className={
           "h-[400px] overflow-auto w-full p-2 mt-2 rounded bg-gray-100"
