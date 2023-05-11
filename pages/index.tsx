9b652ce5b92ff1729e59de9af3dd1b4719c90169
Manohar <itsmanoharkumar@gmail.com>
2023-05-11 10:48:18 +0530

    removed image from home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,12 @@
-import Image from "next/image";
-import dataEntryHeroImage from "@/images/dataEntryHeroImage.jpg";
-import { useState } from "react";
-import axios from "axios";
-
 export default function Home() {
   return (
-    <main className={`flex items-center justify-center w-full h-full`}>
-      <div className={"w-[70%] lg:w-[40%]"}>
-        <Image src={dataEntryHeroImage} alt={"Welcome to Data Entry"} />
-      </div>
+    <main className={`flex justify-center w-full h-full`}>
+      <div className={"text-center"}>Welcome</div>
+      {/*<Image*/}
+      {/*  src={dataEntryHeroImage}*/}
+      {/*  alt={"Welcome to Data Entry"} */}
+      {/*  objectFit={"cover"}*/}
+      {/*/>*/}
     </main>
   );
 }
