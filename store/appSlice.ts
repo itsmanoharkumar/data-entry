f155cb37b36833d32bd059a46f798c59b8de8502
Manohar <itsmanoharkumar@gmail.com>
2023-05-11 10:47:32 +0530

    Default value for token in app Slice

--- store/appSlice.ts
+++ store/appSlice.ts
@@ -4,7 +4,8 @@ import { HYDRATE } from "next-redux-wrapper";
 const initialState = {
   operatingSystem: "",
   serverBaseUrl: "http://localhost:1337/api",
-  serverAuthToken: "",
+  serverAuthToken:
+    "e315f2ca496aeac21d7c6d9232a7ebc6ba20c5d8abb18dd5c4fcbfc1430bfecfeb448e2f86128b09421ecd0b306fa88f4ca62a6015a1491e09463de617fd8d2f13f8ea94c5ab955c7728111ddf1b919ef9c18e432af78963ce8de9bbb1bc9523adeefb1b3b54a5c792c8539322b89f5ee138f2a239ed0016b3a6bc3e95e13272",
   isSideNavOpen: false,
 };
 
